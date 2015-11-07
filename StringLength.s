ce6b888e532b1fcdb50cd291b3f71a4843ab433f
hooman96 <hooman.mohammadi1996@gmail.com>
2015-11-07 12:18:42 -0800

    string length
    find the length of string
    
    use push and pop stack

---
+++ StringLength.s
@@ -0,0 +1,20 @@
+# find the lenght of string
+
+global _start
+
+.data
+
+str1:
+	.string "hello"
+
+str2:
+	.string "goodbye"
+
+.text
+
+strlength:
+
+_start:
+	push $str1
+	call strlength
+	addl $4, %esp
